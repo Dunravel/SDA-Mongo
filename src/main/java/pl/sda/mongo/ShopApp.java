f8107a24e1051c2f4c66a1b42ce0adb46c82bc77
Marcin Iskra <dunravel@gmail.com>
2019-01-12 09:32:06 +0100

    Initial commit

---
+++ src/main/java/pl/sda/mongo/ShopApp.java
@@ -0,0 +1,22 @@
+package pl.sda.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+
+public class ShopApp {
+    public static void main(String[] args) {
+        MongoClient mongoClient = new MongoClient(
+                new ServerAddress("localhost", 27017),
+                MongoCredential.createCredential("javakrk", "library", "javakrk".toCharArray()),
+                MongoClientOptions.builder().build()
+        );
+
+        
+
+
+
+        mongoClient.close();
+    }
+}

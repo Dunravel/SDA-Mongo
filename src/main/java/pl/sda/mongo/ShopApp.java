ed8ac6c32ea911a1239059051a89fc373f4485a1
Marcin Iskra <dunravel@gmail.com>
2019-01-12 09:41:50 +0100

    Extracted Mongo DB connection t oseperate class

--- src/main/java/pl/sda/mongo/ShopApp.java
+++ src/main/java/pl/sda/mongo/ShopApp.java
@@ -4,14 +4,16 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import pl.sda.mongo.mongodb.MongoClientConnectivity;
 
 public class ShopApp {
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient(
-                new ServerAddress("localhost", 27017),
-                MongoCredential.createCredential("javakrk", "library", "javakrk".toCharArray()),
-                MongoClientOptions.builder().build()
-        );
+
+        MongoClientConnectivity mongoClientConnectivity = new MongoClientConnectivity();
+        mongoClientConnectivity.open();
+
+        mongoClientConnectivity.close();
+
 
 
 
